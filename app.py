697bb963cbf9116f2f28b5d2dc1a5c3a3d27f48b
snekha <snekhabaskar29@gmail.com>
2025-06-18 13:23:08 +0530

    Add requirements and main app file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -39,6 +40,8 @@ def delete_task(id):
     return jsonify({'message': 'Task deleted'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
